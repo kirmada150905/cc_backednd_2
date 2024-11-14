72d9a9b36197edba1e084b83be5d7453a99b7208
kirmada150905 <74258997+kirmada150905@users.noreply.github.com>
2024-11-14 22:17:15 +0530

    improved code and added file input to handler func

--- main.go
+++ main.go
@@ -4,12 +4,11 @@ import (
 	"bufio"
 	"encoding/json"
 	"fmt"
-	"strconv"
-	// "text/scanner"
-	"log"
+
+	// "log"
 	"net/http"
 	"os"
-	// "reflect"
+	"strconv"
 )
 
 type Data struct {
@@ -73,131 +72,136 @@ func get_campus(id string) string {
 	}
 
 }
-func handler(w http.ResponseWriter, r *http.Request) {
+
+type FilePath struct {
+	Path string
+}
+
+func (path *FilePath) handler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	file, err := os.Open("./data.txt") // For read access.
+	file, err := os.Open(path.Path) // For read access.
 	if err != nil {
-		log.Fatal(err)
+		w.Write([]byte(err.Error()))
 	}
+	fileInfo, err := file.Stat()
+	if err != nil {
+		w.Write([]byte("Unable to get file info"))
+		// w.Write({"Unable to get file info": http.StatusInternalServerError})
+		return
+	}
+	fileSize := fileInfo.Size()
 
 	scanner := bufio.NewScanner(file)
 	queries := r.URL.Query()
+	var ids []string //array for storing id's
 
 	//{{base_url}}
-	if r.URL.Path == "/" {
-		if len(queries) == 0 {
-			var ids []string //array for storing id's
-			for scanner.Scan() {
-				line := scanner.Text()
-				ids = append(ids, line)
-			}
-
-			dataStruct := Data{Ids: ids} //struct for storing id's
-			//encoding dataStruct into JSON
-			jsonStr, err := json.Marshal(dataStruct)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Write(jsonStr)
-		}
-
-		//{{base_url}}?format=text
-		if queries.Get("format") == "text" {
-			//getting file size
-			fileInfo, err := file.Stat()
-			if err != nil {
-				http.Error(w, "Unable to get file info", http.StatusInternalServerError)
-				return
-			}
-			fileSize := fileInfo.Size()
-
-			file_data := make([]byte, fileSize)
-			_, err = file.Read(file_data)
-			if err != nil {
-				http.Error(w, "Error reading file", http.StatusInternalServerError)
-				return
-			}
-
-			w.Header().Set("Content-Type", "text/plain")
-			w.Write(file_data)
-
-		} else if queries.Get("branch") != "" { //{{base_url}}?branch=X
-			scanner := bufio.NewScanner(file)
-			var ids []string //array for storing id's
-			for scanner.Scan() {
-				line := scanner.Text()
-				if get_brach(line) == queries.Get("branch") {
-					ids = append(ids, line)
-				}
-			}
-
-			dataStruct := Data{Ids: ids} //struct for storing id's
-
-			//encoding dataStruct into JSON
-			jsonStr, err := json.Marshal(dataStruct)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Write(jsonStr)
-		} else if queries.Get("year") != "" { //{{base_url}}?year=X
-			fmt.Println("path: ", r.URL.Path)
-			X, _ := strconv.Atoi(queries.Get("year"))
-			scanner := bufio.NewScanner(file)
-			var ids []string //array for storing id's
-			for scanner.Scan() {
-				line := scanner.Text()
-				year, _ := strconv.Atoi(line[0:4]) //slicing oart of the line(id) that represents year
-				if year == 2024+1-X {
-					ids = append(ids, line)
-					fmt.Println(line)
-				}
-			}
-
-			dataStruct := Data{Ids: ids} //struct for storing id's
-
-			//encoding dataStruct into JSON
-			jsonStr, err := json.Marshal(dataStruct)
-			if err != nil {
-				http.Error(w, err.Error(), http.StatusInternalServerError)
-				return
-			}
-			w.Write(jsonStr)
-		}
-	} else if r.URL.Path != "/" { //{{base_url}}/:id
-		fmt.Println("here")
-		scanner := bufio.NewScanner(file)
+	if len(queries) == 0 {
 		for scanner.Scan() {
 			line := scanner.Text()
-			if line[8:12] == r.URL.Path[1:] {
-				type data struct {
-					Id details `json:"id"`
-				}
-				year, _ := strconv.Atoi(line[0:4])
-				year = 2025 - year
-				brach := get_brach(line)
-				campus := get_campus(line)
-				email := "f" + line[0:4] + line[8:12] + "@" + campus + ".bits-pilani.ac.in"
-				id := line
-				uid := line[8:12]
-				id_deail := details{Year: year, Branch: brach, Campus: campus, Email: email, Id: id, Uid: uid}
-				response := data{Id: id_deail}
-				jsonStr, err := json.Marshal(response)
-				if err != nil {
-					http.Error(w, err.Error(), http.StatusInternalServerError)
-					return
-				}
-				w.Write(jsonStr)
+			ids = append(ids, line)
+		}
+
+		dataStruct := Data{Ids: ids} //struct for storing id's
+		//encoding dataStruct into JSON
+		jsonStr, err := json.Marshal(dataStruct)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonStr)
+	} else if queries.Get("format") == "text" { //{{base_url}}?format=text
+
+		//getting file size
+
+		file_data := make([]byte, fileSize)
+		_, err = file.Read(file_data)
+		if err != nil {
+			http.Error(w, "Error reading file", http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "text/plain")
+		w.Write(file_data)
+
+	} else if queries.Get("branch") != "" { //{{base_url}}?branch=X
+		for scanner.Scan() {
+			line := scanner.Text()
+			if get_brach(line) == queries.Get("branch") {
+				ids = append(ids, line)
 			}
 		}
+
+		dataStruct := Data{Ids: ids} //struct for storing id's
+
+		//encoding dataStruct into JSON
+		jsonStr, err := json.Marshal(dataStruct)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonStr)
+	} else if queries.Get("year") != "" { //{{base_url}}?year=X
+		X, _ := strconv.Atoi(queries.Get("year"))
+		for scanner.Scan() {
+			line := scanner.Text()
+			year, _ := strconv.Atoi(line[0:4]) //slicing oart of the line(id) that represents year
+			if year == 2024+1-X {
+				ids = append(ids, line)
+				fmt.Println(line)
+			}
+		}
+
+		dataStruct := Data{Ids: ids} //struct for storing id's
+
+		//encoding dataStruct into JSON
+		jsonStr, err := json.Marshal(dataStruct)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Write(jsonStr)
+	} else {
+		w.Write([]byte("error"))
 	}
 
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
+}
 
+func (path *FilePath) id_Handler(w http.ResponseWriter, r *http.Request) {
+	file, _ := os.Open(path.Path) // For read access.
+
+	scanner := bufio.NewScanner(file)
+	w.Header().Set("Content-Type", "application/json")
+
+	response_sent := false
+	for scanner.Scan() {
+		line := scanner.Text()
+		if line[8:12] == r.URL.Path[1:] {
+			type data struct {
+				Id details `json:"id"`
+			}
+			year, _ := strconv.Atoi(line[0:4])
+			year = 2025 - year
+			brach := get_brach(line)
+			campus := get_campus(line)
+			email := "f" + line[0:4] + line[8:12] + "@" + campus + ".bits-pilani.ac.in"
+			id := line
+			uid := line[8:12]
+			id_deail := details{Year: year, Branch: brach, Campus: campus, Email: email, Id: id, Uid: uid}
+			response := data{Id: id_deail}
+			jsonStr, err := json.Marshal(response)
+			if err != nil {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+				return
+			}
+			w.Write(jsonStr)
+			response_sent = true
+			break
+		}
+	}
+	if !response_sent {
+		w.Write([]byte("id not found"))
+	}
 }
 
 func main() {
@@ -206,9 +210,12 @@ func main() {
 		Name string `json:"name"`
 		Age  int    `json:"age"`
 	}
+	path := FilePath{Path: "./data.txt"}
+	mux := http.NewServeMux()
 
-	http.HandleFunc("/", handler)
-	http.ListenAndServe(":8080", nil)
+	mux.HandleFunc("/", path.handler)
+	mux.HandleFunc("/{id}", path.id_Handler)
+	http.ListenAndServe(":8080", mux)
 
 }
 
